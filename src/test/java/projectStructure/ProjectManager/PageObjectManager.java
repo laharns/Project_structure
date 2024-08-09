ec25f0c6bc0b81b9b6cb609180d9788de9772e12
laharns <lahar.innovious@gmail.com>
2024-08-09 14:17:40 +0530

    Second

---
+++ src/test/java/projectStructure/ProjectManager/PageObjectManager.java
@@ -0,0 +1,36 @@
+package projectStructure.ProjectManager;
+
+import org.openqa.selenium.WebDriver;
+import projectStructure.PageObject.Forgot;
+import projectStructure.PageObject.Login;
+import projectStructure.PageObject.Registration;
+
+public class PageObjectManager {
+    public WebDriver driver;
+    public Login loginPage;
+    public Registration registrationPage;
+    public Forgot forgotPage;
+
+    public PageObjectManager(WebDriver driver) {
+        this.driver = driver;
+    }
+    public Login getLoginPage() {
+        if (loginPage == null) {
+            loginPage = new Login(driver);
+        }
+        return loginPage;
+    }
+    public Registration geregistrationtPage() {
+        if (registrationPage == null) {
+            registrationPage = new Registration(driver);
+        }
+        return registrationPage;
+    }
+    public Forgot getforgotPage() {
+        if (forgotPage == null) {
+            forgotPage = new Forgot(driver);
+        }
+        return forgotPage;
+    }
+
+}
